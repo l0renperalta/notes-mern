884b7cdf15c9a7cd053986ff74ae3d6b2fd66f8a
l0renperalta <stinpc@hotmail.com>
2023-02-01 16:08:34 -0500

    deploy config

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db');
@@ -15,6 +16,15 @@ app.use(errorHandler);
 // Routes
 app.use('/api/notes', require('./routes/noteRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Server frontend
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolver(__dirname, '../','frontend','build','index.html'));
+  })
+};
 
 // Starting server
 app.listen(app.get('port'), () => console.log(`Server listening in port ${app.get('port')}`));
