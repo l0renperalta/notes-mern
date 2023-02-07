856a5740c843c2c3d6c3bef0555d5f9d747396c4
l0renperalta <stinpc@hotmail.com>
2023-02-06 22:36:35 -0500

    edit handler bugs, almost done

--- frontend/src/components/ItemNote.jsx
+++ frontend/src/components/ItemNote.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { deleteNote } from '../features/notes/noteSlice'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteNote, editNote } from '../features/notes/noteSlice';
 
 function ItemNote({ note }) {
+   const [statusEdit, setStatusEdit] = useState(false);
+   const [values, setValues] = useState({ title: '', description: '' });
+   const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+   const onChange = (e) => {
+      setValues((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+   };
 
-  return (
-    <div className="goal">
-      <button type="button" onClick={() => dispatch(deleteNote(note._id))} className="close">X</button>
-      <h2>{note.title}</h2> 
-      <h3>{note.description}</h3>
-      <div>
-        {new Date(note.createdAt).toLocaleString('en-US')}
+   const handleOnKeyDown = (e) => {
+      if (e.keyCode === 13 && values.title !== '' && values.description !== '') {
+         dispatch(editNote({ values, noteId: note._id }));
+         setStatusEdit(false);
+      }
+      if (e.keyCode === 13 && values.title === '' && values.description !== '') {
+         dispatch(editNote({ title: note.title, description: values.description, noteId: note._id }));
+         setStatusEdit(false);
+      }
+      if (e.keyCode === 13 && values.title !== '' && values.description === '') {
+         dispatch(editNote({ title: values.title, description: note.description, noteId: note._id }));
+         setStatusEdit(false);
+      }
+   };
+
+   return (
+      <div className="goal">
+         <button type="button" onClick={() => dispatch(deleteNote(note._id))} className="close">
+            X
+         </button>
+         {statusEdit ? (
+            <div className="form-group">
+               <input
+                  defaultValue={note.title}
+                  name="title"
+                  onChange={onChange}
+                  onKeyDown={(e) => handleOnKeyDown(e)}
+               />
+               <input
+                  defaultValue={note.description}
+                  name="description"
+                  onChange={onChange}
+                  onKeyDown={(e) => handleOnKeyDown(e)}
+               />
+            </div>
+         ) : (
+            <>
+               <h2 onClick={() => setStatusEdit(true)}>{note.title}</h2>
+               <h3 onClick={() => setStatusEdit(true)}>{note.description}</h3>
+            </>
+         )}
+         <div>{new Date(note.createdAt).toLocaleString('en-US')}</div>
       </div>
-    </div>
-  )
+   );
 }
 
-export default ItemNote
+export default ItemNote;
--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,41 +1,53 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = '/api/notes';
+const API_URL = '/api/notes/';
 
 const getNotes = async (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  }
-  const response = await axios.get(API_URL, config);
-  return response.data;
-}
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   };
+   const response = await axios.get(API_URL, config);
+   return response.data;
+};
 
 const createNote = async (noteData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  }
-  const response = await axios.post(API_URL, noteData, config);
-  return response.data;
-}
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   };
+   const response = await axios.post(API_URL, noteData, config);
+   return response.data;
+};
+
+const editNote = async (values, noteId, token) => {
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   };
+   const response = await axios.put(API_URL + noteId, values, config);
+   return response.data;
+};
 
 const deleteNote = async (noteId, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  }
-  const response = await axios.delete(API_URL + '/' + noteId, config);
-  return response.data;
-}
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   };
+   const response = await axios.delete(API_URL + noteId, config);
+   console.log(response);
+   return response.data;
+};
 
 const noteService = {
-  getNotes,
-  createNote,
-  deleteNote,
-}
+   getNotes,
+   createNote,
+   editNote,
+   deleteNote,
+};
 
 export default noteService;
